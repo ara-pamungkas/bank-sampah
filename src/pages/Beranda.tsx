ec52ad21cb165677c2104953a10c13baa45fe4f0
Flavianus Ara Pamungkas <ara081200@gmail.com>
2023-10-02 22:21:21 +0700

    Update

--- src/pages/Beranda.tsx
+++ src/pages/Beranda.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import "../style/Beranda.css"
 
 function Beranda() {
   return (
-    <div>
-      <h1>
-        Hello from Beranda!
-      </h1>
+    <div id='beranda' className='col'>
+      <div>
+      <h5>
+        Beranda
+      </h5>
+      </div>
+      <div id='beranda-main' className='container p-2 bg-success'>
+        <div className='blok col-md-3 bg-warning'>
+          Total Nasabah
+        </div>
+        <div className='blok col-md-3 bg-secondary'>
+          Pengurus
+        </div>
+        <div className='blok col-md-3 bg-primary'>
+          Transaksi
+        </div>
+      </div>
     </div>
   )
 }
